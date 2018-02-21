aa47d54c84f4697fa374fc62b38af846f23ecc19
Juan Miguel Garcia Perez <juan-miguel.garcia-perez@hpe.com>
2018-02-21 11:25:30 +0100

    Other commit

--- applicacion-simple/src/main/java/com/juanmi/aplicacion/Main.java
+++ applicacion-simple/src/main/java/com/juanmi/aplicacion/Main.java
@@ -3,16 +3,19 @@ package com.juanmi.aplicacion;
 import java.util.HashMap;
 import java.util.Map;
 import org.fluentd.logger.FluentLogger;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication	
 public class Main {
 
-	private static FluentLogger LOG = FluentLogger.getLogger("fluentd.test");
+	private static FluentLogger LOG = FluentLogger.getLogger("app", "fluentd-ui-persistent-noel.192.168.99.100.nip.io", 24224);
+	//LOCAL DOCKER: private static FluentLogger LOG = FluentLogger.getLogger("app", "192.168.99.101", 24224);
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         Map<String, Object> data = new HashMap<String, Object>();
         data.put("from", "userA");
         data.put("to", "userB");
-        LOG.log("follow", data);		
-
+        LOG.log("follow", data);
 	}
 }
